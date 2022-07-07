15ff5a51034bc753013757c81a01da7b068f3251
David Burke <davidgerburke@gmail.com>
2022-07-07 22:59:30 +0100

    folder tidy up

--- B737-800MPD.py
+++ B737-800MPD.py
@@ -1,83 +1,104 @@
 # Importing
-import numpy as np
+import re
 import pandas as pd
-
-# Entering aircraft type
-type = "800"
+import numpy as np
 
 # Loading data from excel
 SnP = pd.read_excel('data\mpdsup.xls', "SYSTEMS AND POWERPLANT MAINTENA", skiprows=6, names=["Revision", "Index", "MPD ITEM NUMBER", "AMM REFERENCE", "CAT", "TASK", "INTERVAL THRES.", "INTERVAL REPEAT", "ZONE", "ACCESS", "APPLICABILITY APL", "APPLICABILITY ENG", "MAN-HOURS", "TASK DESCRIPTION"])
 Str = pd.read_excel('data\mpdsup.xls', "STRUCTURAL MAINTENANCE PROGRAM", skiprows=5, names=["Revision", "Index", "MPD ITEM NUMBER", "AMM REFERENCE", "PGM", "ZONE", "ACCESS", "INTERVAL THRES.", "INTERVAL REPEAT", "APPLICABILITY APL", "APPLICABILITY ENG", "MAN-HOURS", "TASK DESCRIPTION"])
 Zon = pd.read_excel('data\mpdsup.xls', "ZONAL INSPECTION PROGRAM", skiprows=5, names=["Revision", "Index", "MPD ITEM NUMBER", "AMM REFERENCE", "ZONE", "ACCESS", "INTERVAL THRES.", "INTERVAL REPEAT", "APPLICABILITY APL", "APPLICABILITY ENG", "MAN-HOURS", "TASK DESCRIPTION"])
 
-def Appliability(df, path):
-    df.insert(int(len(list(df))), "APPLICABILITY", np.NaN)
-    ListApp = df["APPLICABILITY APL"]
-    ListAppEng = df["APPLICABILITY ENG"]
-    checker = df["APPLICABILITY APL"].isnull()
-    App = df["APPLICABILITY"].copy()
+# Setting up Applicability option
+Applicabilitytable = ["ALL", "NOTE", "600", "700", "700C", "700IGW", "800", "800BCF", "900", "900ER"]
 
-    LenApp = int(len(df["APPLICABILITY"]))
-    RangeApp = list(range(0 , LenApp))
+############################ Functions ############################
+# Defining fuction to be used on all 3 MPD tabs
+def ApplicabilityChecker(table, path):
 
-    for i in RangeApp:
-        ListAppCheck = str(ListApp[i]).split("\n")
-        ListAppEngCheck = str(ListAppEng[i]).split("\n")
+    ############################ Functions ############################
+    # function for searching the applicability function
+    # Checker will match only if it is a full match
+    Checker = lambda y, value: [bool(re.fullmatch(y, x)) for x in value]
+    # Searcher will match if the value is in the provided list
+    Searcher = lambda y, value: [bool(re.search(y, x, re.IGNORECASE)) for x in value]
 
-        if checker[i] == True:
-            App[i] = np.NaN
+    ############################ TRUTH TABLE ############################
+    # Setting up the truth Table
+    # Blank Database
+    truthTable = pd.DataFrame()
+    # Inserting all the unique values from the Applicability APL column
+    truthTable.insert(column = 'APPLICABILITY APL', loc = int(len(truthTable.columns)), value = table['APPLICABILITY APL'].apply(str).unique())
+
+    # Setting up dictionary with applicabilities split
+    # Creating key pairs with the unique values of Applicability as the Key and a blank list as the pair
+    Applic = dict.fromkeys(table["APPLICABILITY APL"].apply(str).unique(), list([]))
+    # For each unique valuse of Applicability / each Key
+    for value in table["APPLICABILITY APL"].apply(str).unique():
+        # Split the value into each a list sperated by new lines
+        split = str(value).split("\n")
+        # Attach this list as the pair to its corrisponding key
+        Applic["{}".format(value)] = list(split)
+    
+    # Creating the applicability table
+    # Cycling through Each Applicability aircraft type
+    for value in Applicabilitytable:
+        # Create a blank list
+        truthColumn = []
+        # Using the applicability dictionary for the line split applicabilities
+        for x in Applic:
+            #  using checker to find full matches between the Applicability aircraft type And the split list from the key pairs
+            theCheck = Checker(value, Applic["{}".format(x)])
+            # if statement to return ture / false to any matches
+            if True in theCheck:
+                truthColumn.append(True)
+            else:
+                truthColumn.append(False)
+        # Appending the resulting list to the Truth Table with the header being the Applicability aircraft type 
+        truthTable.insert(column = '{}'.format(value), loc = int(len(truthTable.columns)), value = truthColumn)
+
+    # Merge Truthtable
+    # Merginging the resulting table of all the tests to the full MPD sheet
+    # The tables are matched as both contain the APPLICABILITY APL Column
+    MPDTruthTable = pd.merge(table, truthTable, on = "APPLICABILITY APL", how = 'inner')
+
+    ############################ Airplane Note ############################
+    Airplane = MPDTruthTable['TASK DESCRIPTION'].apply(str)
+    thetest = Searcher('AIRPLANE NOTE:', Airplane)
+    thelist = []
+    n=0
+    for value in thetest:
+        if value == True:
+            result = re.search(r"AIRPLANE NOTE:(.*)", Airplane[n])
+            thelist.append(result.group(0))
+            #fulllist.append(result.group(0))
+            n += 1 
         else:
-            if ("ALL" in ListAppCheck) and ("NOTE" not in ListAppCheck) and ("ALL" in ListAppEngCheck) and ("NOTE" not in ListAppEngCheck):
-                App[i] = "Applicable"
-            elif (type in ListAppCheck) and ("NOTE" not in ListAppCheck)and ("ALL" in ListAppEngCheck) and ("NOTE" not in ListAppEngCheck):
-                App[i] = "Applicable"
-            elif ("ALL" in ListAppCheck) and ("NOTE" in ListAppCheck) and ("ALL" in ListAppEngCheck) and ("NOTE" not in ListAppEngCheck):
-                App[i] = "Note"
-            elif (type in ListAppCheck) and ("NOTE" in ListAppCheck) and ("ALL" in ListAppEngCheck) and ("NOTE" not in ListAppEngCheck):
-                App[i] = "Note"
-            elif ("ALL" in ListAppCheck) and ("NOTE" not in ListAppCheck) and ("ALL" in ListAppEngCheck) and ("NOTE" in ListAppEngCheck):
-                App[i] = "Note"
-            elif (type in ListAppCheck) and ("NOTE" not in ListAppCheck) and ("ALL" in ListAppEngCheck) and ("NOTE" in ListAppEngCheck):
-                App[i] = "Note"
-            elif (type in ListAppCheck) and ("NOTE" not in ListAppCheck) and ("ALL" in ListAppEngCheck) and ("NOTE" in ListAppEngCheck):
-                App[i] = "Note"
-            elif ("ALL" not in ListAppCheck) and (type not in ListAppCheck):
-                App[i] = "Not Applicable"
+            thelist.append(False)
+            n += 1
+    MPDTruthTable.insert(column = 'AIRPLANE NOTE:', loc = int(len(MPDTruthTable.columns)), value = thelist)
 
-    df["APPLICABILITY"] = App       
-    df.to_excel(path)
 
-Appliability(SnP, "SnP.xlsx")
-Appliability(Str, "Str.xlsx")
-Appliability(Zon, "Zon.xlsx")
+    ############################ APPLICABILITY ############################
+    # Insert blank Row for Applicability
+    MPDTruthTable.insert(column = 'Applicability', loc = int(len(MPDTruthTable.columns)), value = np.nan)
 
-NoteList = []
-def Notegetter(df):
-    Description = df['TASK DESCRIPTION'].copy()
-    LenDes = int(len(Description))
-    RangeDes = list(range(0 , LenDes))
+    # Applying Applicability Rules
+    MPDTruthTable.loc[MPDTruthTable["ALL"] == True, 'Applicability'] = True
+    MPDTruthTable.loc[MPDTruthTable["800"] == True, 'Applicability'] = True
+    MPDTruthTable.loc[MPDTruthTable["NOTE"] == True, 'Applicability'] = "NOTE"
+    MPDTruthTable.loc[(MPDTruthTable["ALL"] == False) & (MPDTruthTable["800"] == False), 'Applicability'] = False
 
-    for x in RangeDes:
-        ListDes = str(Description[x]).split("\n")
-        RangeListDes = list(range(0 , int(len(ListDes))))
+    #print(thelist)
 
-        for i in RangeListDes:
-            if "AIRPLANE NOTE:" in ListDes[i]:
-                NoteList.append(ListDes[i])
-            elif "ENGINE NOTE:" in ListDes[i]:
-                NoteList.append(ListDes[i])
 
-Notegetter(SnP)
-Notegetter(Str)
-Notegetter(Zon)
+    MPDTruthTable.to_excel(path)
 
-NoteShort = []
+############################ Main Code ############################
+fulllist = []
 
-for x in NoteList:
-    # check if exists in unique_list or not
-    if x not in NoteShort:
-        NoteShort.append(x)
-        print(x)
-
-print(len(NoteList))
-print(len(NoteShort))
+ApplicabilityChecker(SnP, "SnPTruthTable.xlsx")
+ApplicabilityChecker(Str, "StrTruthTable.xlsx")
+ApplicabilityChecker(Zon, "ZonTruthTable.xlsx")
+#fulllistdf = pd.DataFrame()
+#fulllistdf.insert(column = 'fulllist', loc = int(len(fulllistdf.columns)), value = fulllist)
+#fulllistdf.to_excel("fulllistdf.xlsx")
